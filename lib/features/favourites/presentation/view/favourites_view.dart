73de44960ab9716f9767e29030bf45070ef92e24
Diana Alejandra Silva Alvarez <diana.silva@otrofy.com>
2026-03-09 09:14:31 -0500

    feat: added favourites view implementation

--- lib/features/favourites/presentation/view/favourites_view.dart
+++ lib/features/favourites/presentation/view/favourites_view.dart
@@ -12,10 +12,35 @@ class _FavouritesViewState extends ConsumerState<FavouritesView> {
   Widget build(BuildContext context) {
     AppLocalizations localizations = AppLocalizations.of(context)!;
 
-    return ErrorIllustrationHome(
-      title: localizations.error_title_no_favourite,
-      subtitle: localizations.error_description_no_favourites,
-      illustration: AssetsConstants.illustrationMagikarpError,
+    Set<int> favorites = ref.watch(favouritesProvider);
+
+    if (favorites.isEmpty) {
+      return ErrorIllustrationHome(
+        title: localizations.error_title_no_favourite,
+        subtitle: localizations.error_description_no_favourites,
+        illustration: AssetsConstants.illustrationMagikarpError,
+      );
+    }
+
+    return Padding(
+      padding: const EdgeInsets.only(top: UILayout.medium),
+      child: ListView.builder(
+        padding: const EdgeInsets.symmetric(horizontal: UILayout.medium),
+        itemCount: favorites.length,
+        itemBuilder: (BuildContext context, int index) {
+          int id = favorites.elementAt(index);
+
+          return Padding(
+            padding: const EdgeInsets.only(bottom: UILayout.smallText),
+            child: PokemonCard(
+              id: id.toString(),
+              name: 'Pokemon',
+              imageUrl:
+                  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png',
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/features/favourites/presentation/view/views.dart
+++ lib/features/favourites/presentation/view/views.dart
@@ -6,5 +6,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/internacionalization/l10n/app_localizations.dart';
 import '../../../../design_system/components/error_illustration_home.dart';
 import '../../../../design_system/illustrations/assets_icons.dart';
+import '../../../../design_system/ui_constants/layout.dart';
+import '../../../dashboard/presentation/viewmodel/favourites_viewmodel.dart';
+import '../../../dashboard/presentation/widgets/widgets.dart';
 
 part 'favourites_view.dart';
